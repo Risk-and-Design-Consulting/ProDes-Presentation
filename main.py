19aaa202828d9269864937f7e7b908ee670c28c8
Julián David Parra Cardona <80989074+juliandp30@users.noreply.github.com>
2022-01-31 17:48:57 -0500

    Update main.py

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ def graficar_data(data, font_size=20):
     )
     fig.update_layout(font_size=font_size)
     fig.update_layout(
-        yaxis_range=[0.98 * min(data["PesoTotalMin"]), 1.02 * max(data["PesoTotalMin"])]
+        yaxis_range=[0.99 * min(data["PesoTotalMin"]), 1.01 * max(data["PesoTotalMin"])]
     )
     st.plotly_chart(fig, use_container_width=True)
 
@@ -83,7 +83,7 @@ def graficar_data(data, font_size=20):
     )
     fig.update_layout(font_size=font_size)
     fig.update_layout(
-        yaxis_range=[0.95 * min(data["TenorTotalMin"]), 1.05 * max(data["TenorTotalMin"])]
+        yaxis_range=[0.99 * min(data["TenorTotalMin"]), 1.01 * max(data["TenorTotalMin"])]
     )
     st.plotly_chart(fig, use_container_width=True)
 
@@ -111,7 +111,7 @@ def graficar_data(data, font_size=20):
     )
     fig.update_layout(font_size=font_size)
     fig.update_layout(
-        yaxis_range=[0.98 * min(data["NúmeroFiguras"]), 1.02 * max(data["NúmeroFiguras"])]
+        yaxis_range=[0.99 * min(data["NúmeroFiguras"]), 1.01 * max(data["NúmeroFiguras"])]
     )
     st.plotly_chart(fig, use_container_width=True)
 
